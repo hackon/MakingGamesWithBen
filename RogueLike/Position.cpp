708e99cee5fa0bb89bf626e3040f9d2e35acf750
hackon <haakon.heiberg@gmail.com>
2015-08-23 19:13:58 +0200

    missing files

---
+++ RogueLike/Position.cpp
@@ -0,0 +1,11 @@
+﻿#include "Position.h"
+
+Position::Position()
+{
+}
+
+Position::Position(int x, int y)
+{
+	_x = x;
+	_y = y;
+}
---
+++ RogueLike/Position.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Position.h"
+
+
+class Position
+{
+public:
+	Position();
+	Position(int x, int y);
+	int getX() { return _x; }
+	int getY() { return _y; }
+private:
+	int _x;
+	int _y;
+};
