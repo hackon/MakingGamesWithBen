7e0723bc8db0229b160d179ce0e8415fe69f8499
hackon <haakon.heiberg@gmail.com>
2015-08-23 14:22:12 +0200

    gatcha

--- Getch/main.cpp
+++ Getch/main.cpp
@@ -8,9 +8,11 @@ using namespace std;
 int main()
 {
 	char input = ' ';
+	string clearScreen(100, '\n');
 	while (input != 'q')
 	{
 		input = _getch();
+		cout << clearScreen;
 		printf("%c", input);
 	}	
 
